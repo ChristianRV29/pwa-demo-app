074684df436dff643b2b3347e23aab109b13e54b
Christian Riano <christianrv.dev@gmail.com>
2025-02-19 21:21:40 -0500

    fix: update absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-  resolve: {
-    alias: {
-      "@/api": './src/api',
-      "@/components": "./src/components",
-      "@/hooks": "./src/hooks",
-      "@/pages": "./src/pages",
-      "@/utils": "./src/utils",
-    }
-  }
+	plugins: [tsconfigPaths(), react()],
+	resolve: {
+		alias: {
+			'@/api': './src/api',
+			'@/components': './src/components',
+			'@/hooks': './src/hooks',
+			'@/layouts': './src/layouts',
+			'@/pages': './src/pages',
+			'@/theme': './src/theme',
+			'@/utils': './src/utils',
+		},
+	},
 })
