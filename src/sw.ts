de1d596f522bc252dd82fcf57183ff9640a55a7e
Christian Riano <christianrv.dev@gmail.com>
2025-02-21 17:02:03 -0500

    feature: PWA configuration

---
+++ src/sw.ts
@@ -0,0 +1,18 @@
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
+import { registerRoute } from 'workbox-routing'
+import { NetworkFirst } from 'workbox-strategies'
+
+declare const self: ServiceWorkerGlobalScope
+
+cleanupOutdatedCaches()
+
+precacheAndRoute(self.__WB_MANIFEST)
+
+registerRoute(
+	({ url }) => {
+		return url.pathname.includes('https://jsonplaceholder.typicode.com'), 'GET'
+	},
+	new NetworkFirst({
+		cacheName: 'todos-cache',
+	}),
+)
