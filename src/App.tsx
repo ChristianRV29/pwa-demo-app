5f075a6c68bf2f6123dfbac880975ee24518c67e
Christian Riano <christianrv.dev@gmail.com>
2025-02-20 20:29:40 -0500

    feature: set up SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { Dashboard } from "@pages"
+import { TodoList, ToolBar } from '@components'
+import { useTodos } from '@hooks'
+import { MainLayout } from '@layouts'
 
 function App() {
-  return (
-    <Dashboard />
-  )
+	const {
+		todos,
+		searchTerm,
+		currentFilter,
+		setCurrentFilter,
+		setSearchTerm,
+		toggleCompleted,
+	} = useTodos()
+
+	return (
+		<MainLayout>
+			<ToolBar
+				currentFilter={currentFilter}
+				searchTerm={searchTerm}
+				setCurrentFilter={setCurrentFilter}
+				setSearchTerm={setSearchTerm}
+			/>
+			<TodoList onCompleted={toggleCompleted} todos={todos} />
+		</MainLayout>
+	)
 }
 
 export default App
